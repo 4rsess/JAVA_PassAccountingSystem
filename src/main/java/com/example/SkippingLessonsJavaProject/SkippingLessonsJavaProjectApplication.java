651473760a984b8700a1e678d7482bcfef288e96
4rsess <misha.d.nosov@gmail.com>
2025-03-24 20:50:26 +0700

    PassAccountingSystem перевел проект в GitHub

--- src/main/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplication.java
+++ src/main/java/com/example/SkippingLessonsJavaProject/SkippingLessonsJavaProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkippingLessonsJavaProjectApplication {
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SkippingLessonsJavaProjectApplication.class, args);
 	}
